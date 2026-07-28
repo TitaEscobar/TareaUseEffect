135e17cef07cf1ee2f730619b37593dc35ac5af5
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-07-27 21:03:10 -0600

    - definiendo componente reutilizable customButton - implementando componente personalizado customButton en App.tsx

--- MyRoutineApp/App.tsx
+++ MyRoutineApp/App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from './src/components/CustomButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      //implementacion de componente personalizado
+      <CustomButton/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ MyRoutineApp/src/components/CustomButton.tsx
@@ -0,0 +1,11 @@
+import { Button, Text, View } from "react-native";
+//definicion de componente personalizado utilizando componentes nativos
+export default function CustomButton () {
+
+    return(
+        <View>
+            <Text> Titulo Cualquiera de Boton </Text>
+            <Button title="4"/>
+        </View>
+    );
+}
