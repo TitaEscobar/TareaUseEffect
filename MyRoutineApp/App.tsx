9a6c35c327d25ab3cd016c659389085247572e72
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-08-24 20:16:25 -0600

    implementacion de objeto de referencia a la prop ref del componente de navegacion

--- MyRoutineApp/App.tsx
+++ MyRoutineApp/App.tsx
@@ -1,10 +1,11 @@
+import { navigationRef } from "./src/navigation/NavigationService";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
         <StackNavigator />
     </NavigationContainer>
   );
