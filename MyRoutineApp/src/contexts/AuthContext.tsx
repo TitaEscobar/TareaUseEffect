d396b28f09d601169015a9571f7dc91e17f78490
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-08-26 20:36:20 -0600

    - definir tipo de funcion login en el contexto - creacion del hook personalizado useAuth - definicion de logica de funciones login y logout en el AuthProvider

--- MyRoutineApp/src/contexts/AuthContext.tsx
+++ MyRoutineApp/src/contexts/AuthContext.tsx
@@ -1,31 +1,43 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 
 //1. Tipado del objeto principal del contexto
 type User = {
     email: string;
     pwd?: string;
-} 
+} | null
 
 type AuthContextType ={
     user: User | null;
-    login: () => {};
+    login: (email: string) => boolean;
     logout: () => void;
 }
 
 //2. Creacion del contexto
 const AuthContext = createContext<AuthContextType | null> (null);
 
+// 4. hook personalizado: la exposicion de contexto a componentes de la aplicacion
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if(!context) throw new Error ("useAuth debe usarse dentro de AuthProvider");
+    return context;
+}
 
 //3. Crear el provider: medio por el cual manejamos el estado global
 export const AuthProvider = ({children}: {children: React.ReactNode}) => {
     
-    const [user, setUser] = useState(null);
-    const login =() =>{
-        return false;
+    const [user, setUser] = useState<User>(null);
+    
+    const login = (correo: string): boolean =>{
+        const isAllowed = correo.endsWith('.edu')
+         if (isAllowed){
+            setUser({email: correo})
+         }
+
+        return isAllowed;
     };
     const logout = () =>{
-
+        setUser(null);
     };
 
     return(
