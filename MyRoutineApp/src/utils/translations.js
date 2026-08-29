c2c518824d3a64d8dde1cfa99b23720f3f904fa6
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-08-28 20:35:09 -0600

    agregando diccionario de traducciones

---
+++ MyRoutineApp/src/utils/translations.js
@@ -0,0 +1,15 @@
+export const translations ={
+    en: {
+        signIn: "Sign In",
+        typeEmail: "Type your email address",
+        invalidEmail: "Invalid email address",
+        welcomeLogin: "Welcome to Login"
+    },
+
+    es: {
+        signIn: "Iniciar Sesion",
+        typeEmail: "Ingresa tu correo electronico",
+        invalidEmail: "Correo invalido",
+        welcomeLogin: "Bienvenido a Login"
+    },
+};
